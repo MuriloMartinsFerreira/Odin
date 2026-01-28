9c3e14e67967e5a0a1cbbb0a7839b19087ab5fb3
Murilo <murilommferreira18@gmail.com>
2026-01-28 18:20:58 -0300

    Sketchpad finalizado com opção de apagar e escolher diferentes cores

--- JavaScript/sketchpad/sketch.js
+++ JavaScript/sketchpad/sketch.js
@@ -1,15 +1,29 @@
+const colorPicker = document.getElementById('colorPicker');
+
 function createSketchpad() {
     const gridSize = 120 * 64;
     const grid = document.getElementById('sketchpad');
+    let mouseDown = false;
+
+    document.body.onmousedown = () => (mouseDown = true);
+    document.body.onmouseup = () => (mouseDown = false);
 
     for (let i = 0; i < gridSize; i++) {
         const pixel = document.createElement('div');
         pixel.classList.add('pixel');
         pixel.addEventListener('mouseover', () => {
-            pixel.style.backgroundColor = 'black';
+            if (mouseDown) {
+                pixel.style.backgroundColor = colorPicker.value;
+            }
         });
         grid.appendChild(pixel);
     }
+}
+
+function resetSketchpad() {
+    const grid = document.getElementById('sketchpad');
+    grid.innerHTML = '';
+    createSketchpad();
 }
 
 createSketchpad();
